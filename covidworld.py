71a55dd716732e47c69e5946b3801242c23a8488
djmaulana <53201103+djmaulana@users.noreply.github.com>
2022-06-21 13:47:20 +0700

    Update covidworld.py

--- covidworld.py
+++ covidworld.py
@@ -6,7 +6,7 @@ from bokeh.models import HoverTool
 from bokeh.models.widgets import Tabs, Panel
 
 
-df = pd.read_csv(r'C:\Users\djmaulana\Downloads\visdat\data\full_grouped.csv')
+df = pd.read_csv(r'\data\full_grouped.csv')
 
 import pandas as pd
 from bokeh.plotting import figure
